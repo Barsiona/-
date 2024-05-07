4ede79d3cd03335e665a8b65a0845dccfeb09774
Лидия <angelika_525@inbox.ru>
2024-05-07 19:28:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-import flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
-app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/test_db7'
+app = Flask('Steps')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///steps.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-class Message(db.Model):
+
+class Steps(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(512), nullable=False)
+    steps = db.Column(db.Integer)
+    date = db.Column(db.date)
+    in_stock = db.Column(db.Boolean, default=True)
 
-    def __init__(self, text, tags):
-        self.text = text
-        self.tags = [
-            Tag(text=tag) for tag in tags.split(',')
-        ]
-
-
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(32), nullable=False)
-
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
-    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+    def __repr__(self):
+        return f'Product{self.id}. {self.steps} - {self.date}'
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    return flask.render_template('index.html', messages=Message.query.all())
+@app.route('/')
+def main():
+    steps = Steps.query.all()
+    return render_template('index.html', steps_list=steps)
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = flask.request.form['text']
-    tag = flask.request.form['tag']
-    # messages.append(Message(text, tag))
-    db.session.add(Message(text, tag))
+@app.route('/in_stock/<product_id>', methods=['PATCH'])
+def modify_product(product_id):
+    product = Product.query.get(product_id)
+    product.in_stock = request.json['in_stock']
     db.session.commit()
 
-    return flask.redirect(flask.url_for('hello'))
 
+@app.route('/add', methods=['POST'])
+def add_product():
+    data = request.json
+    product = Product(**data)
+    db.session.add(product)
+    db.session.commit()
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
 
